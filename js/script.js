b30b9fd01e0c26fabe64cb3650241d27f34038c2
Adrian Leniart <adrianleniart@gmail.com>
2020-07-01 15:35:04 +0200

    currency switch added

--- js/script.js
+++ js/script.js
@@ -6,8 +6,44 @@ const resetButton = document.querySelector(".js-resetButton");
 let resultExchange = document.querySelector(".js-resultExchange");
 
 const ratePLN = 1.0;
-const rateEUR = 4.40;
 const rateUSD = 3.92;
-const rateGBP = 4.04;
+const rateEUR = 4.40;
+const rateGBP = 4.83;
 let result;
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  switch (selectFrom.value) {
+    case "PLN":
+      result = +amount.value * ratePLN;
+      break;
+    case "EUR":
+      result = +amount.value * rateEUR;
+      break;
+    case "USD":
+      result = +amount.value * rateUSD;
+      break;
+    case "GBP":
+      result = +amount.value * rateGBP;
+      break;
+  }
+
+  switch (selectTo.value) {
+    case "PLN":
+      result /= ratePLN;
+      break;
+    case "EUR":
+      result /= rateEUR;
+      break;
+    case "USD":
+      result /= rateUSD;
+      break
+    case "GBP":
+      result /= rateGBP;
+      break;
+  }
+
+  resultExchange.innerText = result.toFixed(2)
+});
 
