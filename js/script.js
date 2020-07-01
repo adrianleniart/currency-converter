73d0710d6f6d8ec61fe5d064434d183e67087b64
Adrian Leniart <adrianleniart@gmail.com>
2020-07-01 14:44:48 +0200

    added query selector

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const amount = document.querySelector(".js-amount");
+const selectFrom = document.querySelector(".js-selectFrom");
+const selectTo = document.querySelector(".js-selectTo");
+const submitButton = document.querySelector(".js-submitButton");
+const resetButton = document.querySelector(".js-resetButton");
+let resultExchange = document.querySelector(".js-resultExchange");
+
+const ratePLN = 1.0;
+const rateEUR = 4.40;
+const rateUSD = 3.92;
+const rateGBP = 4.04;
+let result;
+
